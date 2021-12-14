d4ee540e3148779bf9c3e4c67df0c43aab0e890a
Luke Stevens <lcm.stevens96@gmail.com>
2021-12-14 08:57:31 +0000

    Day 12 and 13 solutions

--- src/day12/Day12.kt
+++ src/day12/Day12.kt
@@ -2,26 +2,99 @@ package day12
 
 import checkAnswer
 import readInput
+import toPair
 
 fun main() {
-    fun part1(input: List<Long>): Long {
-        return input.size.toLong()
+    val start = "start"
+    val end = "end"
+
+
+
+    fun processPaths(
+        point: String,
+        routes: Map<String,Set<String>>,
+        paths: MutableSet<List<String>>,
+        currentPath: List<String>,
+        ignoreSmallCave: (String, List<String>) -> Boolean
+    ){
+        val newCurrentPath = currentPath.toMutableList().apply { add(point) }
+        if(point == end){
+            paths.add(newCurrentPath)
+        }
+        else if (
+            point.lowercase() == point && ignoreSmallCave(point, currentPath)){
+            return
+        }
+        else if(!paths.contains(newCurrentPath)){
+            paths.add(newCurrentPath)
+            val nextPoints = routes[point]?: listOf()
+            nextPoints.forEach {
+                processPaths(it, routes, paths, newCurrentPath, ignoreSmallCave)
+            }
+        }
     }
 
-    fun part2(input: List<Long>): Long {
-        return input.size.toLong()
+    fun part1(input: Map<String,MutableSet<String>>): Int {
+        val paths = mutableSetOf<List<String>>()
+        processPaths(start, input, paths, mutableListOf()){ point, path ->
+            path.contains(point)
+        }
+        return paths.filter { it.contains(end) }.count()
     }
 
-    fun getInput(file: String): List<Long> {
-        return readInput(file)[0].split(",").map { it.toLong() }
+    fun part2(input: Map<String,MutableSet<String>>): Int {
+        val paths = mutableSetOf<List<String>>()
+        processPaths(start, input, paths, mutableListOf()){ point, path ->
+            if(!path.contains(point)){
+                false
+            }
+            else if(point == start){
+                true
+            }
+            else {
+                // find all small caves on path, and make a map of the number of times visited
+                path.filter { it.lowercase() == it }
+                    .fold(mutableMapOf<String, Int>()) { map, p ->
+                        map.merge(p, 1) { count, _ -> count + 1 }
+                        map
+                    }.maxOf { it.value } > 1 // if any are visited more than once, then ignore
+            }
+        }
+        return paths.filter { it.contains(end) }.count()
+    }
+
+    fun getInput(file: String): Map<String,MutableSet<String>> {
+        return readInput(file).map {
+            it.split("-").toPair()
+        }.fold(mutableMapOf()){ map, path ->
+            map.computeIfAbsent(path.first){ mutableSetOf() }
+                .add(path.second)
+            // also add inverse relation
+            map.computeIfAbsent(path.second){ mutableSetOf() }
+                .add(path.first)
+            map
+        }
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = getInput("day12/test")
-    checkAnswer(part1(testInput), 1)
-    //checkAnswer(part2(testInput), 1)
+    val testInput1 = getInput("day12/test1")
+    checkAnswer(part1(testInput1), 10)
+    checkAnswer(part2(testInput1), 36)
+
+    val testInput2 = getInput("day12/test2")
+    checkAnswer(part1(testInput2), 19)
+    checkAnswer(part2(testInput2), 103)
+
+    val testInput3 = getInput("day12/test3")
+    checkAnswer(part1(testInput3), 226)
+    checkAnswer(part2(testInput3), 3509)
 
     val input = getInput("day12/input")
     println(part1(input))
     println(part2(input))
+}
+
+fun MutableSet<List<String>>.print(){
+    forEach{ println(it.joinToString(",")) }
+    println("-".repeat(20))
 }
--- src/day13/Day13.kt
+++ src/day13/Day13.kt
@@ -1,27 +1,88 @@
 package day13
 
 import checkAnswer
+import common.Point
 import readInput
+import toPair
+import java.lang.IllegalStateException
 
 fun main() {
-    fun part1(input: List<Long>): Long {
-        return input.size.toLong()
+
+    fun fold(points: Set<Point>, fold: Point): MutableSet<Point> {
+        return if(fold.x == 0){
+            points.map {
+                if(it.y > fold.y){
+                    Point(it.x, 2*fold.y - it.y)
+                } else it
+            }.toMutableSet()
+        }
+        else {
+            points.map {
+                if(it.x > fold.x){
+                    Point(2*fold.x - it.x, it.y)
+                } else it
+            }.toMutableSet()
+        }
     }
 
-    fun part2(input: List<Long>): Long {
-        return input.size.toLong()
+
+    fun part1(input: Pair<List<Point>,List<Point>>): Long {
+        var points = input.first.toMutableSet()
+
+        // only do first fold
+        val fold = input.second[0]
+        points = fold(points, fold)
+
+        return points.size.toLong()
     }
 
-    fun getInput(file: String): List<Long> {
-        return readInput(file)[0].split(",").map { it.toLong() }
+    fun part2(input: Pair<List<Point>,List<Point>>): Long {
+        var points = input.first.toMutableSet()
+
+        input.second.forEach { fold ->
+            points = fold(points, fold)
+        }
+
+        // print it
+        val maxX = points.maxOf { it.x }
+        val maxY = points.maxOf { it.y }
+        for(y in 0..maxY){
+            for(x in 0..maxX){
+                print(
+                    if(points.contains(Point(x, y))) "#" else " "
+                )
+            }
+            println()
+        }
+
+        return 0L
+    }
+
+    fun getInput(file: String): Pair<List<Point>,List<Point>> {
+        val points = mutableListOf<Point>()
+        val folds = mutableListOf<Point>()
+        readInput(file).forEach {
+            if(it.startsWith("fold along")){
+                val num = it.substring(13).toInt()
+                folds.add(when(it[11]){
+                    'y' -> Point(0, num)
+                    'x' -> Point(num, 0)
+                    else -> throw IllegalStateException("Bad char ${it[11]}")
+                })
+            }
+            else if(it.isNotEmpty()){
+                points.add(it.split(",").map { it.toInt() }.let { Point(it[0], it[1]) })
+            }
+        }
+        return Pair(points, folds)
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = getInput("day13/test")
-    checkAnswer(part1(testInput), 1)
+    checkAnswer(part1(testInput), 17)
     //checkAnswer(part2(testInput), 1)
 
     val input = getInput("day13/input")
     println(part1(input))
-    println(part2(input))
+    part2(input)
 }
