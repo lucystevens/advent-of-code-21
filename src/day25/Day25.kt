6c68fe6817195716893b5ce72e6698f759651fd4
lukecmstevens <lukecmstevens@users.noreply.github.com>
2021-12-25 08:19:19 +0000

    Generated files for day 25

---
+++ src/day25/Day25.kt
@@ -0,0 +1,27 @@
+package day25
+
+import checkAnswer
+import readInput
+
+fun main() {
+    fun part1(input: List<Long>): Long {
+        return input.size.toLong()
+    }
+
+    fun part2(input: List<Long>): Long {
+        return input.size.toLong()
+    }
+
+    fun getInput(file: String): List<Long> {
+        return readInput(file)[0].split(",").map { it.toLong() }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = getInput("day25/test")
+    checkAnswer(part1(testInput), 1L, "testInput part1")
+    //checkAnswer(part2(testInput), 1L, "testInput part2")
+
+    val input = getInput("day25/input")
+    println(part1(input))
+    println(part2(input))
+}
